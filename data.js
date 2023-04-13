22e129f7c624b9b8d8139317e51ac752d6eefed2
tims-001 <130621800+tims-001@users.noreply.github.com>
2023-04-12 21:44:58 -0400

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+window.reviews = [
+{ 
+    name: "Divyraj Shah",
+    date: "12/04/23",
+    rating: 4,
+    text: "The game is mind-blowing.Found it tough in the starting but its pretty much challenging.",
+},
+{
+    name: "Riple Kuvadiya",
+    date: "12/04/23",
+    rating: 5,
+    text: "i loved this game.Playing it since childhood.Challenge you to defeat me!!",
+},
+{
+    name: "Abhay Thakur",
+    date: "10/04/23",
+    rating: 3,
+    text: "The game is hard.Personally, what i think is to increase vocabulary, we can read novels because there is a story in it and it also increases creativity.",
+},
+
+{
+    name: "Lakshay Singhania",
+    date: "11/04/23",
+    rating: 1,
+    text: "I am not interested in such games. I love virtual games like pubg,fortnite and so may which are so exciting and so cool.",
+},            
+{
+    name: "Mehr Panchal",
+    date: "12/04/23",
+    rating: 5,
+    text: "It's one of my favourite game because i love creating new words and using it in new sentences.",
+},
+]
+
